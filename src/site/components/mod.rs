61e0493d51832b17a9ea7be4a67655fde73013a8
Dennis Chen <dchen@mathadvance.org>
2024-05-20 20:46:41 -0700

    use favicon.ico instead

--- src/site/components/mod.rs
+++ src/site/components/mod.rs
@@ -13,7 +13,7 @@ pub fn html(title: &str, email: Option<String>, body: &str) -> String {
                 <title>{}</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <link rel="stylesheet" href="/main.css">
-                <link rel="icon" href="/favicon.svg">
+                <link rel="icon" href="/favicon.ico">
             </head>
             <body>
                 {}
