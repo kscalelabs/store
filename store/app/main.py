4a5869eccde99e7a6ab62760d2c15f0910333f3e
Benjamin Bolte <ben@bolte.cc>
2024-05-20 23:12:01 -0700

    minimal test

--- store/app/main.py
+++ store/app/main.py
@@ -7,12 +7,16 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-FRONTEND_DIR = (Path(__file__).parent / "frontend").resolve()
-if not (FRONTEND_BUILD_DIR := FRONTEND_DIR / "build").exists():
-    raise FileNotFoundError(f"Frontend is not built to {FRONTEND_BUILD_DIR}")
+# FRONTEND_DIR = (Path(__file__).parent / "frontend").resolve()
+# if not (FRONTEND_BUILD_DIR := FRONTEND_DIR / "build").exists():
+#     raise FileNotFoundError(f"Frontend is not built to {FRONTEND_BUILD_DIR}")
 
 
 # Mounts the static frontend files to the /static path.
-app.mount("/", StaticFiles(directory=FRONTEND_BUILD_DIR, html=True), name="static")
+# app.mount("/", StaticFiles(directory=FRONTEND_BUILD_DIR, html=True), name="static")
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
 application = app  # For WSGI compatibility
