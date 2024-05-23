89305ee45efe7783347ca7c19481da3181d8e0e1
Dennis Chen <dchen@mathadvance.org>
2024-05-22 21:46:16 -0700

    Put all config in env files
    Typically I don't like doing this and prefer config files,
    but this project is meant to go up on AWS. Especially since docker files
    LOOOVE env variables...

--- src/backend/config.rs
+++ src/backend/config.rs
@@ -1,10 +1,7 @@
-use crate::dirs::config_path;
-use derivative::Derivative;
 use serde::{Deserialize, Serialize};
-use std::fs::read_to_string;
 
 /// General configuration
-#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Config {
     /// Used for cookies as well as links included in automated emails
     pub domain: String,
@@ -29,18 +26,15 @@ impl Default for Ports {
 }
 
 /// Used when creating the connection pool.
-#[derive(Clone, Debug, Derivative, Deserialize, Serialize)]
-#[derivative(Default)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Postgres {
     /// Address of host of Postgres database.
     /// If you intend to host your Postgres database on the same machine as the website, this
     /// should be localhost.
-    #[derivative(Default(value=r#"String::from("localhost")"#))]
     pub host: String,
     /// Name of the database that stores all of your store instance's data.
     pub dbname: String,
     /// Postgres username (used for access control to the database)
-    #[derivative(Default(value=r#"String::from("postgres")"#))]
     pub user: String,
     /// Postgres password (used for access control to the database)
     pub password: String,
@@ -53,7 +47,7 @@ pub struct Postgres {
 ///
 /// In case of the latter, the sysadmin will be able to read any replies.
 
-#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Mail {
     /// Domain where the mailserver is hosted.
     pub relay: String,
@@ -71,7 +65,6 @@ pub struct Mail {
 impl Config {
     /// Gets configuration from the designated configuration path (see the config_path function)
     pub fn get() -> Self {
-        toml::from_str(&read_to_string(config_path()).unwrap())
-            .unwrap_or_else(|e| panic!("There were errors deserializing the configuration: {}", e))
+        envy::from_env::<Config>().unwrap()
     }
 }
