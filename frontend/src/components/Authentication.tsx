cf9b4624fdf567402f4091efd9a0f4deadc5807b
Benjamin Bolte <ben@bolte.cc>
2024-05-28 16:55:29 -0400

    login section improvements

--- frontend/src/components/Authentication.tsx
+++ frontend/src/components/Authentication.tsx
@@ -1,35 +1,57 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
+import { Button, Form, InputGroup } from "react-bootstrap";
+import Overlay from "react-bootstrap/Overlay";
+import { Tooltip } from "react-bootstrap";
+
+const isValidEmail = (email: string) => {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+};
 
 const Authentication = () => {
   const [email, setEmail] = useState<string | null>(null);
   const [authenticated, setAuthenticated] = useState<boolean>(false);
 
+  const target = useRef(null);
+
   return (
-    <div>
+    <div style={{ maxWidth: 400 }}>
       {authenticated ? (
-        <>
-          <span>{email ?? ""}</span>
-          <button
-            style={{ marginLeft: 10 }}
+        <InputGroup className="mb-3">
+          <InputGroup.Text>{email}</InputGroup.Text>
+          <Button
+            variant="outline-secondary"
             onClick={() => setAuthenticated(false)}
           >
             Log Out
-          </button>
-        </>
+          </Button>
+        </InputGroup>
       ) : (
-        <>
-          <input
-            type="text"
-            value={email ?? ""}
-            onChange={(e) => setEmail(e.target.value)}
+        <InputGroup className="mb-3">
+          <InputGroup.Text>✉️</InputGroup.Text>
+          <Form.Control
+            placeholder="Email"
+            aria-label="email"
+            value={email || ""}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
           />
-          <button
-            style={{ marginLeft: 10 }}
+          <Button
+            variant="outline-secondary"
             onClick={() => setAuthenticated(true)}
+            disabled={email === null || !isValidEmail(email)}
+            ref={target}
           >
             Log In
-          </button>
-        </>
+          </Button>
+          <Overlay
+            placement="right"
+            show={email !== null && !isValidEmail(email)}
+            target={target.current}
+          >
+            {(props) => <Tooltip {...props}>Invalid email</Tooltip>}
+          </Overlay>
+        </InputGroup>
       )}
     </div>
   );
