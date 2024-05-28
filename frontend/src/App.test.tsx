d9f626cabac9723bca51399bee7e232b216944a9
Ben Bolte <ben@bolte.cc>
2024-05-28 16:57:30 -0400

    separate robots page vs individual robot (#8)

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -4,6 +4,6 @@ import App from "./App";
 
 test("renders stompy urdf link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/MJCF/i);
+  const linkElement = screen.getByText(/robolist.xyz/i);
   expect(linkElement).toBeInTheDocument();
 });
