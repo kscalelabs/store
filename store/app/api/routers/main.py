b971a5254be3d6db654b551519e8236f8fdf991a
Benjamin Bolte <ben@bolte.cc>
2024-05-29 16:01:54 -0400

    http 404 endpoint

--- store/app/api/routers/main.py
+++ store/app/api/routers/main.py
@@ -2,7 +2,7 @@
 
 import logging
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 
 from store.app.api.routers.users import users_router
 
@@ -11,3 +11,9 @@ logger = logging.getLogger(__name__)
 api_router = APIRouter()
 
 api_router.include_router(users_router, prefix="/users", tags=["users"])
+
+
+# Returns a 404 response for all other paths.
+@api_router.get("/{path:path}")
+async def not_found(path: str) -> dict[str, str]:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Endpoint not found")
