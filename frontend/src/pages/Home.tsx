a1c702c3fd33dbb198d585ae37ad12c17b05b4ca
Benjamin Bolte <ben@bolte.cc>
2024-05-30 22:57:48 -0700

    re-clean up home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,32 +5,30 @@ const RobotDetails = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="pt-5 rounded-lg">
-      <Col>
-        <Row className="mb-5">
-          <h1 className="display-4">robolist</h1>
-          <p className="lead">Buy and sell robots and robot parts</p>
-        </Row>
-        <Row>
-          <Col md={6} sm={12} className="mt-2">
-            <Card onClick={() => navigate(`/robots`)}>
-              <Card.Body>
-                <Card.Title>Robots</Card.Title>
-                <Card.Text>Buy and sell robot</Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col md={6} sm={12} className="mt-2">
-            <Card onClick={() => navigate(`/parts`)}>
-              <Card.Body>
-                <Card.Title>Parts</Card.Title>
-                <Card.Text>Buy and sell robot parts</Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Col>
-    </div>
+    <Col className="pt-5">
+      <Row className="mb-5">
+        <h1 className="display-4">robolist</h1>
+        <p className="lead">Buy and sell robots and robot parts</p>
+      </Row>
+      <Row>
+        <Col md={6} sm={12} className="mt-2">
+          <Card onClick={() => navigate(`/robots`)}>
+            <Card.Body>
+              <Card.Title>Robots</Card.Title>
+              <Card.Text>Buy and sell robot</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col md={6} sm={12} className="mt-2">
+          <Card onClick={() => navigate(`/parts`)}>
+            <Card.Body>
+              <Card.Title>Parts</Card.Title>
+              <Card.Text>Buy and sell robot parts</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Col>
   );
 };
 
