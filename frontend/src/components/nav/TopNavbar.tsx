2f79ceaab46a5b2074e970c6bc393aba7c8f3d32
Benjamin Bolte <ben@bolte.cc>
2024-05-30 18:02:19 -0700

    icon tooltips

--- frontend/src/components/nav/TopNavbar.tsx
+++ frontend/src/components/nav/TopNavbar.tsx
@@ -1,13 +1,19 @@
 import Sidebar from "components/nav/Sidebar";
 import { useTheme } from "hooks/theme";
-import { useState } from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { useRef, useState } from "react";
+import { Container, Nav, Navbar, Overlay, Tooltip } from "react-bootstrap";
 import { GearFill, MoonFill, SunFill } from "react-bootstrap-icons";
 import { Link } from "react-router-dom";
 
 const TopNavbar = () => {
   const [showSidebar, setShowSidebar] = useState<boolean>(false);
   const { theme, setTheme } = useTheme();
+
+  const themeRef = useRef(null);
+  const sidebarRef = useRef(null);
+
+  const [showThemeTooltip, setShowThemeTooltip] = useState<boolean>(false);
+  const [showSidebarTooltip, setShowSidebarTooltip] = useState<boolean>(false);
 
   return (
     <>
@@ -21,12 +27,38 @@ const TopNavbar = () => {
             <Nav>
               <Nav.Link
                 onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+                ref={themeRef}
+                onMouseEnter={() => setShowThemeTooltip(true)}
+                onMouseLeave={() => setShowThemeTooltip(false)}
               >
                 {theme === "dark" ? <MoonFill /> : <SunFill />}
               </Nav.Link>
-              <Nav.Link onClick={() => setShowSidebar(true)}>
+              <Overlay
+                placement="bottom-end"
+                show={showThemeTooltip}
+                target={themeRef.current}
+              >
+                {(props) => (
+                  <Tooltip {...props}>
+                    Toggle {theme === "dark" ? "light" : "dark"} mode
+                  </Tooltip>
+                )}
+              </Overlay>
+              <Nav.Link
+                onClick={() => setShowSidebar(true)}
+                ref={sidebarRef}
+                onMouseEnter={() => setShowSidebarTooltip(true)}
+                onMouseLeave={() => setShowSidebarTooltip(false)}
+              >
                 <GearFill />
               </Nav.Link>
+              <Overlay
+                placement="bottom-end"
+                show={showSidebarTooltip}
+                target={sidebarRef.current}
+              >
+                {(props) => <Tooltip {...props}>Settings</Tooltip>}
+              </Overlay>
             </Nav>
           </Navbar.Collapse>
         </Container>
