78d0b498970287054ce7301849c9dc4551c02743
Dennis Chen <dchen@mathadvance.org>
2024-06-01 13:37:05 -0700

    add default smtp port 587
    realistically it will never NOT be 587

--- store/settings/environment.py
+++ store/settings/environment.py
@@ -22,7 +22,7 @@ class UserSettings:
 @dataclass
 class EmailSettings:
     host: str = field(default=MISSING)
-    port: int = field(default=MISSING)
+    port: int = field(default=587)
     email: str = field(default=MISSING)
     password: str = field(default=MISSING)
     name: str = field(default=MISSING)
