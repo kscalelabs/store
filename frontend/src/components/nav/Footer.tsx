316f1c2f2fac4146c627bd0ae7301a5ddbe1b300
Ben Bolte <ben@bolte.cc>
2024-06-03 13:10:32 -0700

    link to github (#41)

---
+++ frontend/src/components/nav/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="fixed-bottom">
+      {/* Solid background */}
+      <div className="text-center bg-body-tertiary p-2">
+        <a href="mailto:support@robolist.xyz">support@robolist.xyz</a> |{" "}
+        <a href="https://github.com/kscalelabs/store">Github</a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
