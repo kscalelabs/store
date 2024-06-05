03778cfa3eb4879f92161e5ef4f9be782424b99f
Dennis Chen <dchen@mathadvance.org>
2024-06-05 15:51:07 -0700

    Make signout button

--- frontend/src/components/auth/AuthComponent.tsx
+++ frontend/src/components/auth/AuthComponent.tsx
@@ -1,8 +1,27 @@
-import { Col, Row } from "react-bootstrap";
+import { useAuthentication } from "hooks/auth";
+import { Button, Col, Row } from "react-bootstrap";
 import EmailAuthComponent from "./EmailAuthComponent";
 import GoogleAuthComponent from "./GoogleAuthComponent";
 
+
 const AuthComponent = () => {
+  const { isAuthenticated, api } = useAuthentication();
+
+  if (isAuthenticated) {
+    return (
+      <Row>
+        <Col xs="auto">
+          <Button onClick={() => {
+            api.delete<boolean>("/users/logout");
+          }}>
+            <span style={{ display: "flex", alignItems: "center" }}>
+              Sign Out
+            </span>
+          </Button>
+        </Col>
+      </Row>
+    );
+  }
   return (
     <>
       <Row>
