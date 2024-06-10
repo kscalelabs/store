88e373176b995e75e5a182b8ffc4175bb83cdc0f
Dennis Chen <41879777+chennisden@users.noreply.github.com>
2024-06-10 15:18:36 -0700

    NavBar UI improvement (#95)
    * NavBar UI improvement
    
    * format

--- frontend/src/components/nav/TopNavbar.tsx
+++ frontend/src/components/nav/TopNavbar.tsx
@@ -16,19 +16,16 @@ const TopNavbar = () => {
           <Navbar.Brand as={Link} to="/">
             robolist
           </Navbar.Brand>
-          <Navbar.Toggle />
-          <Navbar.Collapse className="justify-content-end">
-            <Nav>
-              <Nav.Link
-                onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-              >
-                {theme === "dark" ? <MoonFill /> : <SunFill />}
-              </Nav.Link>
-              <Nav.Link onClick={() => setShowSidebar(true)}>
-                <GearFill />
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
+          <div className="d-flex gap-3">
+            <Nav.Link
+              onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+            >
+              {theme === "dark" ? <MoonFill /> : <SunFill />}
+            </Nav.Link>
+            <Nav.Link onClick={() => setShowSidebar(true)}>
+              <GearFill />
+            </Nav.Link>
+          </div>
         </Container>
       </Navbar>
       <Sidebar show={showSidebar} onHide={() => setShowSidebar(false)} />
