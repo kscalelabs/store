c749ef058f491180ae480dae5b03e448ff818314
Antony Raj <Antony.Raj@ricoh-usa.com>
2024-07-22 22:04:33 +0530

    remove an unused method

--- store/app/routers/users.py
+++ store/app/routers/users.py
@@ -229,26 +229,6 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     return encoded_jwt
 
 
-async def get_current_user(token: str = Depends(get_session_token)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except JWTError:
-        raise credentials_exception
-    user = get_user(fake_users_db, username=token_data.username)
-    if user is None:
-        raise credentials_exception
-    return user
-
-
 @users_router.post("/login")
 async def login_user_endpoint(
     data: UserLogin,
