56edbeb0666bad14c5753c1b8d9d13ed907aec5a
Dennis Chen <dchen@mathadvance.org>
2024-07-25 17:17:03 -0700

    Move all relevant robots.py stuff to listings

--- store/app/crud/listings.py
+++ store/app/crud/listings.py
@@ -16,6 +16,12 @@ class ListingsCrud(BaseCrud):
     async def get_listing(self, listing_id: str) -> Listing | None:
         return await self._get_item(listing_id, Listing, throw_if_missing=False)
 
+    async def add_listing(self, listing: Listing) -> None:
+        await self._add_item(listing)
+
+    async def delete_listing(self, listing_id: str) -> None:
+        await self._delete_item(listing_id)
+
     async def get_listing_tag(self, listing_tag_id: str) -> ListingTag | None:
         return await self._get_item(listing_tag_id, ListingTag, throw_if_missing=False)
 
