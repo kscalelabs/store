87c76855ec59d370abbf0a64c9b147dfec07d42c
Senthil Athiban <senthilathiban0@gmail.com>
2024-07-30 13:05:47 +0530

    (feat): add zod signupschema

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -5,5 +5,19 @@ export const LoginSchema = z.object({
     .string()
     .min(1, { message: "Email Required" })
     .email("Invalid Email"),
-  passowrd: z.string().min(4, { message: "Password Required" }),
+  password: z.string().min(4, { message: "Password Required" }),
 });
+
+export const SignUpSchema = z
+  .object({
+    email: z
+      .string()
+      .min(1, { message: "Email Required" })
+      .email("Invalid Email"),
+    password: z.string().min(4, { message: "Password Required" }),
+    confirmPassword: z.string().min(4, { message: "Enter valid password" }),
+  })
+  .refine((data) => data.confirmPassword === data.password, {
+    message: "Password not matched",
+    path: ["confirm"],
+  });
