6653ad1874ec94886a93175cd98282590a664e96
Ben Bolte <ben@bolte.cc>
2024-08-01 22:37:01 -0700

    fix ui bugs (#232)
    * fix ui bugs
    
    * more fixes
    
    * fix background issue too
    
    * final tweak
    
    * neurotic spacing

---
+++ frontend/src/components/listing/ListingBody.tsx
@@ -0,0 +1,30 @@
+import { paths } from "gen/api";
+import ListingArtifacts from "./ListingArtifacts";
+import ListingChildren from "./ListingChildren";
+import ListingDescription from "./ListingDescription";
+
+type ListingResponse =
+  paths["/listings/{id}"]["get"]["responses"][200]["content"]["application/json"];
+
+interface ListingBodyProps {
+  listing: ListingResponse;
+}
+
+const ListingBody = (props: ListingBodyProps) => {
+  const { listing } = props;
+  return (
+    <div className="px-4">
+      <ListingDescription
+        description={listing.description}
+        edit={listing.owner_is_user}
+      />
+      <ListingChildren
+        child_ids={listing.child_ids}
+        edit={listing.owner_is_user}
+      />
+      <ListingArtifacts listing_id={listing.id} edit={listing.owner_is_user} />
+    </div>
+  );
+};
+
+export default ListingBody;
