3a6845787c6f3256f6a0d7ec2d4f75acd28719a7
Benjamin Bolte <ben@bolte.cc>
2024-08-10 01:30:34 -0700

    lint

--- frontend/src/components/listing/renderers/UrdfRenderer.tsx
+++ frontend/src/components/listing/renderers/UrdfRenderer.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */
+// @ts-nocheck
+
 /* eslint-disable react/no-unknown-property */
 import { Suspense, useRef, useState } from "react";
 import { FaTimes } from "react-icons/fa";
