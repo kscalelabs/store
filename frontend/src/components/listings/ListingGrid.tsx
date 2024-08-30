667de13de164fa4065591cd869ac042fc59716f1
Benjamin Bolte <ben@bolte.cc>
2024-08-29 17:13:16 -0700

    better listing grid

--- frontend/src/components/listings/ListingGrid.tsx
+++ frontend/src/components/listings/ListingGrid.tsx
@@ -49,7 +49,7 @@ const ListingGrid = (props: ListingGridProps) => {
       <Spinner />
     </div>
   ) : (
-    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-8 mx-auto">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-8 mx-auto">
       {listingIds.map((listingId) => (
         <ListingGridCard
           key={listingId}
