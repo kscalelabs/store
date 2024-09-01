c73ea2eba691120779d682f3b52e65a8b9f92901
Benjamin Bolte <ben@bolte.cc>
2024-08-31 17:10:19 -0700

    make listings sorted by most upvoted by default

--- frontend/src/components/ui/Select/Select.tsx
+++ frontend/src/components/ui/Select/Select.tsx
@@ -3,11 +3,6 @@ import React from "react";
 import { type VariantProps, cva } from "class-variance-authority";
 import { cn } from "utils";
 
-export interface SelectOption {
-  value: string;
-  label: string;
-}
-
 const selectVariants = cva(
   "flex h-10 w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50",
   {
@@ -29,14 +24,14 @@ const selectVariants = cva(
   },
 );
 
-interface SelectProps
+interface SelectProps<T extends React.Key>
   extends Omit<React.SelectHTMLAttributes<HTMLSelectElement>, "size">,
     VariantProps<typeof selectVariants> {
-  options: SelectOption[];
+  options: { value: T; label: string }[];
   variant?: "default" | "outline";
 }
 
-const Select = React.forwardRef<HTMLSelectElement, SelectProps>(
+const Select = React.forwardRef<HTMLSelectElement, SelectProps<React.Key>>(
   ({ className, options, variant = "default", size, ...props }, ref) => {
     return (
       <select
@@ -45,15 +40,15 @@ const Select = React.forwardRef<HTMLSelectElement, SelectProps>(
         {...props}
       >
         {options.map((option) => (
-          <option key={option.value} value={option.value}>
+          <option key={option.value} value={String(option.value)}>
             {option.label}
           </option>
         ))}
       </select>
     );
   },
-);
-
-Select.displayName = "Select";
+) as <T extends React.Key>(
+  props: SelectProps<T> & { ref?: React.ForwardedRef<HTMLSelectElement> },
+) => JSX.Element;
 
 export { Select, selectVariants };
--- frontend/src/components/ui/TypingAnimation.tsx
+++ frontend/src/components/ui/TypingAnimation.tsx
@@ -12,7 +12,7 @@ interface TypingAnimationProps {
 
 export default function TypingAnimation({
   text,
-  duration = 200,
+  duration = 100,
   className,
 }: TypingAnimationProps) {
   const [displayedText, setDisplayedText] = useState<string>("");
