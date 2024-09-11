7a5e4e4932ca5a3a262eb1f7ce1fb999ae2551d7
Winston Hsiao <hsiaowinston@gmail.com>
2024-09-10 21:26:10 -0400

    light mode fixes

--- frontend/src/components/landing/BuySection.tsx
+++ frontend/src/components/landing/BuySection.tsx
@@ -7,7 +7,7 @@ export default function BuySection() {
       <div className="container mx-auto px-4 md:px-6">
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
           <div className="space-y-2">
-            <div className="inline-block rounded-lg bg-gray-100 px-3 py-1 text-sm dark:bg-gray-800">
+            <div className="inline-block rounded-lg bg-gray-200 px-3 py-1 text-sm dark:bg-gray-800">
               New Release
             </div>
             <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
