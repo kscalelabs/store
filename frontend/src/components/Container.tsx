a8b33b3416f3dd34ea9da73cde4e6dc89724eda6
Winston Hsiao <96440583+Winston-Hsiao@users.noreply.github.com>
2024-09-25 23:06:01 -0400

    Reduce navbar height (#425)

--- frontend/src/components/Container.tsx
+++ frontend/src/components/Container.tsx
@@ -8,7 +8,7 @@ const Container = (props: ContainerProps) => {
   const { children } = props;
 
   return (
-    <div className="my-20 mx-4 sm:mx-6 md:mx-10 xl:mx-16 max-w-full">
+    <div className="my-[68px] mx-4 sm:mx-6 md:mx-10 xl:mx-16 max-w-full">
       {children}
     </div>
   );
