022623d2275b24826559a42da217ad9ea8bdb7cb
WT-MM <wesley.maa@gmail.com>
2024-09-27 15:05:17 -0700

    center div

--- frontend/src/components/pages/MuJoCoTest.tsx
+++ frontend/src/components/pages/MuJoCoTest.tsx
@@ -4,7 +4,7 @@ import MUJOCO from "../listing/mujoco";
 
 const MuJoCoTestPage: React.FC = () => {
   return (
-    <div className="absolute top-0 left-0 w-1/2 h-full">
+    <div className="">
       <MUJOCO url="/examples/scenes/stompypro.xml" />
     </div>
   );
