af1c117b9991dc212cd946605678b1a05ce2030f
Winston Hsiao <hsiaowinston@gmail.com>
2024-10-01 18:06:24 -0400

    Increase font size

--- frontend/src/components/nav/Sidebar.tsx
+++ frontend/src/components/nav/Sidebar.tsx
@@ -19,7 +19,7 @@ const SidebarItem = ({ icon, title, onClick }: SidebarItemProps) => (
   <li>
     <button
       onClick={onClick}
-      className="w-full flex items-center py-2 px-3 text-sm text-gray-1 hover:bg-gray-1 hover:text-primary-9 rounded-md"
+      className="w-full flex items-center py-2 px-3 text-xl text-gray-1 hover:bg-gray-1 hover:text-primary-9 rounded-md"
     >
       {icon && <span className="mr-2">{icon}</span>}
       {title}
@@ -70,7 +70,7 @@ const Sidebar = ({ show, onClose }: SidebarProps) => {
               </button>
             </div>
             <nav>
-              <ul className="space-y-1">
+              <ul className="space-y-1l">
                 {navItems.map((item) => (
                   <SidebarItem
                     key={item.name}
@@ -78,7 +78,7 @@ const Sidebar = ({ show, onClose }: SidebarProps) => {
                     onClick={() => handleItemClick(item.path, item.isExternal)}
                   />
                 ))}
-                <div className="border-t-2 border-gray-1 my-2"></div>
+                <div className="border-t border-gray-1 my-2"></div>
                 {communityItems.map((item) => (
                   <SidebarItem
                     key={item.name}
@@ -86,7 +86,7 @@ const Sidebar = ({ show, onClose }: SidebarProps) => {
                     onClick={() => handleItemClick(item.path, true)}
                   />
                 ))}
-                <div className="border-t-2 border-gray-1 my-2"></div>
+                <div className="border-t border-gray-1 my-2"></div>
                 {isAuthenticated ? (
                   <>
                     <SidebarItem
