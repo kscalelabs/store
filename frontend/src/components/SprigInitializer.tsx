0d0dbdded2fd5c27824c1cc5da7a68b2d56a2f4e
Ivan <45982459+ivntsng@users.noreply.github.com>
2024-10-04 21:21:37 -0700

    439: GDPR Cookie Consent Banner (#449)
    * 439: GDPR Cookie Consent Banner
    
    * Updated GDPR banner text and consent option grouping
    
    * Resolved linting issues
    
    ---------
    
    Co-authored-by: casper <ivntsng@gmail.com>

--- frontend/src/components/SprigInitializer.tsx
+++ frontend/src/components/SprigInitializer.tsx
@@ -8,25 +8,31 @@ const SprigInitializer = () => {
   const { currentUser, isAuthenticated } = useAuthentication();
 
   useEffect(() => {
-    const initializeSprig = () => {
-      if (window.Sprig && typeof window.Sprig === "function") {
-        console.log("Sprig initialized");
+    const sprigConsent = localStorage.getItem("sprig-consent") === "true";
 
-        // Set user ID and email in Sprig if user is authenticated
-        if (isAuthenticated && currentUser) {
-          window.Sprig("setUserId", currentUser.id);
-          window.Sprig("setUserEmail", currentUser.email);
+    if (sprigConsent) {
+      const initializeSprig = () => {
+        if (window.Sprig && typeof window.Sprig === "function") {
+          console.log("Sprig initialized");
+
+          // Set user ID and email in Sprig if user is authenticated
+          if (isAuthenticated && currentUser) {
+            window.Sprig("setUserId", currentUser.id);
+            window.Sprig("setUserEmail", currentUser.email);
+          }
+
+          // Track page view
+          window.Sprig("track", "page_view", { path: location.pathname });
+        } else {
+          console.log("Sprig is not ready yet. Retrying...");
+          setTimeout(initializeSprig, 500);
         }
+      };
 
-        // Track page view
-        window.Sprig("track", "page_view", { path: location.pathname });
-      } else {
-        console.log("Sprig is not ready yet. Retrying...");
-        setTimeout(initializeSprig, 500);
-      }
-    };
-
-    initializeSprig();
+      initializeSprig();
+    } else {
+      console.log("Sprig tracking is disabled due to user preferences.");
+    }
   }, [location.pathname, isAuthenticated, currentUser]);
 
   return null;
---
+++ frontend/src/components/gdpr/gdprbanner.tsx
@@ -0,0 +1,144 @@
+import React, { useEffect, useState } from "react";
+
+const GDPRBanner: React.FC = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [showOptOutForm, setShowOptOutForm] = useState(false);
+  const [sprigConsent, setSprigConsent] = useState(true);
+  const [googleAnalyticsConsent, setGoogleAnalyticsConsent] = useState(true);
+  const [pendoConsent, setPendoConsent] = useState(true);
+
+  useEffect(() => {
+    const consentGiven = localStorage.getItem("gdpr-consent");
+    if (!consentGiven) {
+      setIsVisible(true);
+    }
+  }, []);
+
+  const handleAccept = () => {
+    localStorage.setItem("sprig-consent", "true");
+    localStorage.setItem("google-analytics-consent", "true");
+    localStorage.setItem("pendo-consent", "true");
+    localStorage.setItem("gdpr-consent", "true");
+    setIsVisible(false);
+    window.location.reload();
+  };
+
+  const handleOptOut = () => {
+    setShowOptOutForm(true);
+  };
+
+  const handleSaveOptOutPreferences = () => {
+    localStorage.setItem("sprig-consent", sprigConsent.toString());
+    localStorage.setItem(
+      "google-analytics-consent",
+      googleAnalyticsConsent.toString(),
+    );
+    localStorage.setItem("pendo-consent", pendoConsent.toString());
+    localStorage.setItem("gdpr-consent", "true");
+    setShowOptOutForm(false);
+    setIsVisible(false);
+    window.location.reload();
+  };
+
+  const handleBackToBanner = () => {
+    setShowOptOutForm(false);
+  };
+
+  return (
+    <>
+      {isVisible && !showOptOutForm && (
+        <div className="bg-gray-12 p-4 fixed bottom-0 left-1/2 transform -translate-x-1/2 w-full max-w-lg flex flex-col items-center z-50 shadow-md rounded-lg">
+          <div className="text-gray-1 text-xs sm:text-sm text-center mb-2 max-w-full">
+            We value your privacy 🔒 we use cookies to make it easier to
+            interact with our website and to improve it. We want to better
+            understand how our website is used. You can find out more about our
+            use of cookies in our
+            <a
+              href="https://kscale.store/privacy"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-primary-9 underline ml-1"
+            >
+              Privacy Policy
+            </a>
+            .
+          </div>
+          <div className="flex justify-center gap-2">
+            <button
+              className="bg-gray-12 text-gray-1 border border-gray-7 rounded-full px-4 py-2 text-sm hover:bg-gray-8"
+              onClick={handleOptOut}
+            >
+              Opt out
+            </button>
+            <button
+              className="bg-primary-9 text-white rounded-full px-4 py-2 transition-colors duration-300 hover:bg-primary-8"
+              onClick={handleAccept}
+            >
+              Accept
+            </button>
+          </div>
+        </div>
+      )}
+
+      {showOptOutForm && (
+        <div className="bg-gray-12 p-4 fixed bottom-0 left-1/2 transform -translate-x-1/2 w-full max-w-lg flex flex-col items-center z-50 shadow-md rounded-lg">
+          <div className="text-gray-1 text-sm text-center mb-2 max-w-full">
+            Please select which services you would like to opt out of.
+          </div>
+
+          <div className="flex justify-between w-full px-4 py-2">
+            <label className="text-gray-1 text-sm">Necessary</label>
+            <input
+              type="checkbox"
+              checked={true}
+              disabled
+              className="toggle-checkbox"
+            />
+          </div>
+
+          <div className="flex justify-between w-full px-4 py-2">
+            <label className="text-gray-1 text-sm">Analytics</label>
+            <input
+              type="checkbox"
+              checked={googleAnalyticsConsent}
+              onChange={() =>
+                setGoogleAnalyticsConsent(!googleAnalyticsConsent)
+              }
+              className="toggle-checkbox"
+            />
+          </div>
+
+          <div className="flex justify-between w-full px-4 py-2">
+            <label className="text-gray-1 text-sm">User Experience</label>
+            <input
+              type="checkbox"
+              checked={sprigConsent && pendoConsent}
+              onChange={() => {
+                setSprigConsent(!sprigConsent);
+                setPendoConsent(!pendoConsent);
+              }}
+              className="toggle-checkbox"
+            />
+          </div>
+
+          <div className="flex justify-center gap-2 mt-4">
+            <button
+              className="bg-gray-12 text-gray-1 border border-gray-7 rounded-full px-4 py-2 text-sm hover:bg-gray-8"
+              onClick={handleBackToBanner}
+            >
+              Back
+            </button>
+            <button
+              className="bg-primary-9 text-white rounded-full px-4 py-2 transition-colors duration-300 hover:bg-primary-8"
+              onClick={handleSaveOptOutPreferences}
+            >
+              Save Preferences
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default GDPRBanner;
