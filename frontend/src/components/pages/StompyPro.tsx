d24b023fc0d4b9cad81be80f4a3a53dbeaa307b4
Winston Hsiao <hsiaowinston@gmail.com>
2024-10-16 21:37:09 -0400

    Added checkout buttons to pro and mini pages

--- frontend/src/components/pages/StompyPro.tsx
+++ frontend/src/components/pages/StompyPro.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Container from "@/components/Container";
+import CheckoutButton from "@/components/stripe/CheckoutButton";
 
 const StompyPro: React.FC = () => {
   return (
@@ -8,6 +9,10 @@ const StompyPro: React.FC = () => {
       <div className="flex flex-col items-center justify-center h-full">
         <h1 className="text-2xl font-bold">Stompy Pro</h1>
         <p className="text-xl text-gray-11">PAGE IS UNDER CONSTRUCTION</p>
+        {/* Developer Mode Stompy Pro */}
+        {/* <CheckoutButton productId="prod_Qyzd8f0gFMis7c" /> */}
+        {/* Production Stompy Pro */}
+        <CheckoutButton productId="prod_R0n3nkCO4aQdlg" />
       </div>
     </Container>
   );
