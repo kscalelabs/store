f634ce409bafd8a728527d32adff656742e4be4c
Benjamin Bolte <ben@bolte.cc>
2024-10-17 22:43:34 -0700

    update structure

--- frontend/src/components/pages/Home.tsx
+++ frontend/src/components/pages/Home.tsx
@@ -21,82 +21,13 @@ import {
 
 const Home: React.FC = () => {
   const containerRef = useRef(null);
-  const navigate = useNavigate();
 
   return (
-    <div ref={containerRef} className="bg-gray-1 text-gray-12">
+    <div ref={containerRef} className="bg-gray-1 text-gray-12 space-y-6">
       <HeroSection />
       <StompyProSection />
       <StompyMiniSection />
-      <section className="w-full">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-          {[
-            {
-              title: "Downloads",
-              description: "Kernel images, URDFs, ML models, and more.",
-              icon: DownloadIcon,
-              path: "/downloads",
-              buttonText: "Browse Downloads",
-            },
-            {
-              title: "Browse Builds",
-              description:
-                "Browse robot builds with linked CAD files, part lists, and various related downloads.",
-              icon: MagnifyingGlassIcon,
-              path: "/browse",
-              buttonText: "View Builds",
-            },
-            {
-              title: "Code",
-              description:
-                "Explore or contribute to dozens of open-source repositories from robot builds to edge vision-language-action models, kernel images, simulations, and more.",
-              icon: CodeIcon,
-              path: "https://github.com/kscalelabs",
-              buttonText: "Visit our Github",
-              external: true,
-            },
-            {
-              title: "Discord",
-              description:
-                "Connect with fellow robot enthusiasts, industry experts, and researchers. Share projects and ideas, and stay updated on the latest K-Scale developments.",
-              icon: FaDiscord,
-              path: "https://discord.com/invite/kscale",
-              buttonText: "Join our Discord",
-              external: true,
-            },
-          ].map((item, index) => (
-            <Card
-              key={index}
-              className="cursor-pointer flex flex-col h-full transition-all duration-300 ease-in-out hover:scale-[1.03] hover:shadow-lg bg-gray-12"
-              onClick={() =>
-                item.external
-                  ? window.open(item.path, "_blank")
-                  : navigate(item.path)
-              }
-            >
-              <CardHeader>
-                <CardTitle className="text-gray-2 text-lg sm:text-xl">
-                  {item.title}
-                </CardTitle>
-                <CardDescription className="text-gray-7 font-light">
-                  {item.description}
-                </CardDescription>
-              </CardHeader>
-              <CardContent className="flex-grow flex flex-col justify-end">
-                <item.icon className="w-12 h-12 mb-4 text-primary-9" />
-                <div className="inline-flex items-center text-sm font-medium text-primary-1">
-                  {item.buttonText}
-                  {item.external ? (
-                    <ExternalLinkIcon className="ml-1 h-4 w-4" />
-                  ) : (
-                    <ChevronRightIcon className="ml-1 h-4 w-4" />
-                  )}
-                </div>
-              </CardContent>
-            </Card>
-          ))}
-        </div>
-      </section>
+      <OptionsSection />
     </div>
   );
 };
@@ -140,7 +71,7 @@ const HeroSection: React.FC = () => {
   );
 
   return (
-    <section className="relative overflow-hidden mb-12 h-[40vh] rounded-lg">
+    <section className="relative overflow-hidden h-[40vh] rounded-lg">
       <PageHeader fillGrid={myFillGrid} />
     </section>
   );
@@ -151,7 +82,7 @@ const StompyProSection: React.FC = () => {
 
   return (
     <section
-      className="relative h-[600px] overflow-hidden mb-12 cursor-pointer transition-all duration-300 hover:opacity-90 rounded-lg"
+      className="relative h-[600px] overflow-hidden cursor-pointer transition-all duration-300 hover:opacity-90 rounded-lg"
       onClick={() => navigate("/pro")}
     >
       <div className="absolute inset-0">
@@ -180,7 +111,7 @@ const StompyMiniSection: React.FC = () => {
 
   return (
     <section
-      className="relative h-[600px] overflow-hidden mb-12 cursor-pointer transition-all duration-300 hover:opacity-90 rounded-lg"
+      className="relative h-[600px] overflow-hidden cursor-pointer transition-all duration-300 hover:opacity-90 rounded-lg"
       onClick={() => navigate("/mini")}
     >
       <div className="absolute inset-0">
@@ -199,6 +130,82 @@ const StompyMiniSection: React.FC = () => {
             </h2>
           </div>
         </div>
+      </div>
+    </section>
+  );
+};
+
+const OptionsSection: React.FC = () => {
+  const navigate = useNavigate();
+
+  return (
+    <section className="w-full">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+        {[
+          {
+            title: "Downloads",
+            description: "Kernel images, URDFs, ML models, and more.",
+            icon: DownloadIcon,
+            path: "/downloads",
+            buttonText: "Browse Downloads",
+          },
+          {
+            title: "Browse Builds",
+            description:
+              "Browse robot builds with linked CAD files, part lists, and various related downloads.",
+            icon: MagnifyingGlassIcon,
+            path: "/browse",
+            buttonText: "View Builds",
+          },
+          {
+            title: "Code",
+            description:
+              "Explore or contribute to dozens of open-source repositories from robot builds to edge vision-language-action models, kernel images, simulations, and more.",
+            icon: CodeIcon,
+            path: "https://github.com/kscalelabs",
+            buttonText: "Visit our Github",
+            external: true,
+          },
+          {
+            title: "Discord",
+            description:
+              "Connect with fellow robot enthusiasts, industry experts, and researchers. Share projects and ideas, and stay updated on the latest K-Scale developments.",
+            icon: FaDiscord,
+            path: "https://discord.com/invite/kscale",
+            buttonText: "Join our Discord",
+            external: true,
+          },
+        ].map((item, index) => (
+          <Card
+            key={index}
+            className="cursor-pointer flex flex-col h-full transition-all duration-300 ease-in-out hover:scale-[1.03] hover:shadow-lg bg-gray-12"
+            onClick={() =>
+              item.external
+                ? window.open(item.path, "_blank")
+                : navigate(item.path)
+            }
+          >
+            <CardHeader>
+              <CardTitle className="text-gray-2 text-lg sm:text-xl">
+                {item.title}
+              </CardTitle>
+              <CardDescription className="text-gray-7 font-light">
+                {item.description}
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="flex-grow flex flex-col justify-end">
+              <item.icon className="w-12 h-12 mb-4 text-primary-9" />
+              <div className="inline-flex items-center text-sm font-medium text-primary-1">
+                {item.buttonText}
+                {item.external ? (
+                  <ExternalLinkIcon className="ml-1 h-4 w-4" />
+                ) : (
+                  <ChevronRightIcon className="ml-1 h-4 w-4" />
+                )}
+              </div>
+            </CardContent>
+          </Card>
+        ))}
       </div>
     </section>
   );
