7c03981bc5299356dd24ba56665a6a0700bc4711
Ivan <ivntsng@gmail.com>
2024-11-07 23:55:15 -0800

    unify spinner component styling

--- frontend/src/components/ui/Spinner.tsx
+++ frontend/src/components/ui/Spinner.tsx
@@ -1,10 +1,7 @@
-import { FaSpinner } from "react-icons/fa";
-
 const Spinner = (props: React.HTMLAttributes<HTMLDivElement>) => {
   return (
     <div role="status" {...props}>
-      <FaSpinner className="animate-spin" />
-      <span className="sr-only">Loading...</span>
+      <div className="animate-spin rounded-full h-6 w-6 border-b-2 border-gray-12"></div>
     </div>
   );
 };
